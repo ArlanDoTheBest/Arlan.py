a2ea0e6a7ed0a77013be96232922578cacf0e3cd
ArlanDoTheBest <armakaev74@gmail.com>
2024-01-24 12:35:42 +0600

    Initial Commit

---
+++ Comments.py
@@ -0,0 +1,10 @@
+#1 Exercise
+#This is a comment
+
+#2 Exercise
+'''
+This is a comment
+written in 
+more than just one line
+'''
+
---
+++ DataTypes.py
@@ -0,0 +1,34 @@
+#1 Exercise
+x = 5
+print(type(x))
+#int
+
+#2 Exercise
+x =("Hello world!")
+print(type(x))
+#str
+
+#3 Exercise
+x = 20.5
+print(type(x))
+#float
+
+#4 Exercise
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#list
+
+#5 Exercise
+["apple", "banana", "cherry"]
+print(type(x))
+#tuple
+
+#6 Exercise
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#dict
+
+#7 Exercise
+x = True
+print(type(x))
+#bool
