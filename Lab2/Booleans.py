132ec6098b2f4ee2f7bb161f76e7ac1480df3865
ArlanDoTheBest <156879212+ArlanDoTheBest@users.noreply.github.com>
2024-02-02 23:30:49 +0600

    Add files via upload

---
+++ Lab2/Booleans.py
@@ -0,0 +1,21 @@
+#1 Exercise
+print(10>9)
+True
+
+#2 Exercise
+print(10 == 9)
+False
+
+#3 Exercise
+print(10 < 9)
+False
+
+#4 Exercise
+print(bool("abc"))
+True
+
+#5 Exercise
+print(bool(0))
+False
+
+
